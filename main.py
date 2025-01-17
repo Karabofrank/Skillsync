568d31b560125ec37436aba9e7d596af769128f8
Osmond <osmatsejhb024@student.wethinkcode.co.za>
2025-01-17 15:01:33 +0200

    Firebase intro

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate(r"./Firbase\ key/skillsink-9567a-firebase-adminsdk-fbsvc-2e87c26293.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
 # User ID dictionary
 user_id = {"Name": "",
            "Email": "",
