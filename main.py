3a931498a90b4a8b86091dcfd52b3e34242d4d27
Osmond <osmatsejhb024@student.wethinkcode.co.za>
2025-01-17 14:50:17 +0200

    Firebase intro

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate(r"./Firbase\ key/skillsink-9567a-firebase-adminsdk-fbsvc-2e87c26293.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 # User ID dictionary
 user_id = {"Name": "",
            "Email": "",
@@ -12,7 +20,7 @@ meeting_id ={
     "status": ""
 }
 
-workshop_id ={
+workshop_id = {
     "resource_id": "",
     "topic": "",
     "date_requested": ""
